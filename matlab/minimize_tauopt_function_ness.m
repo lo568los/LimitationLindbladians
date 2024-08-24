951d7688a96d8ec6233f745bf583d543e65027e2
lo568los <soumyadeepsarma3@gmail.com>
2024-08-25 01:56:40 +0530

    non-eq and coh tau

---
+++ matlab/minimize_tauopt_function_ness.m
@@ -0,0 +1,86 @@
+% Code to minimize tau for given parameters. This serves as a numerical
+% check of the possiblity of thermalization..
+
+load("ness_data_NL = 2_NM = 2_1.mat")
+
+%decide paramteres.
+
+NL = 2; % has to be atleast 1
+NM = 2; % has to be atleast 1
+N = NL+NM;
+
+dL = 2^NL;
+dM = 2^NM;
+d = dL*dM;
+
+
+w0list = zeros(N,1) + 1;
+glist = zeros(N-1,1)+ 0.0016;
+
+deltalist = zeros(N-1,1) +1;
+beta = 1;
+
+
+% create_hamiltonian
+H_S = create_hamiltonian(w0list,glist,deltalist,N);
+
+
+
+[V_unsorted,D_unsorted] = eig(H_S); %not ordered. We order them so that they match the ordering
+%obtained from Python. This is not required, but can be useful for checking
+%both MATLAB and Python Code).
+[temp,ind] = sort(diag(D_unsorted));
+V = V_unsorted(:,ind);
+
+F = generate_orthonormal_basis(NL);
+
+if (basis_is_orthonormal(F) == false)
+    warning('Basis NOT orthonormal. Something wrong \n');
+end
+
+
+length_F = length(F); % should be DL^2-1
+
+for index = 1:length_F
+    F{index} = kron(eye(dM)/sqrt(dM), F{index});
+end
+
+
+if (basis_is_orthonormal(F) == false)
+    warning('Basis NOT orthonormal. Something wrong here! \n');
+end
+
+
+%% Starting the SDP.
+
+rho_th = dm_ness;  %setting our non-eq setup rho_th
+
+cvx_begin sdp 
+    cvx_precision high %set CVX precision
+    variable H_LS(dL,dL) hermitian %declare H_LS to be a hermitian matrix.
+    variable gamma_matrix(dL^2-1,dL^2-1) hermitian semidefinite %declare Gamma to be PSD matrix.
+
+    objfunc = 0;
+
+    for i = 1:d
+           objfunc = objfunc+ abs(V(:,i)'*create_L2(rho_th,H_S,H_LS,gamma_matrix,F,NL,NM)*V(:,i)) ;
+    end
+    minimize objfunc %minimize objfunc 
+
+    subject to %constraints..
+        trace(gamma_matrix) == 1;
+        
+cvx_end
+
+%we compute and save the individual values that are summed up to obtain
+%tau. 
+diag_values = zeros(d,1);
+for i = 1:d
+    diag_values(i) = V(:,i)'*create_L2(rho_th,H_S,H_LS,gamma_matrix,F,NL,NM)*V(:,i);
+end
+
+save("thermal_data_1")
+
+%gamma_matrix_approx = round(gamma_matrix,5); % round to 5 places after decimal.
+%xlswrite("gamma_matrix.xlx",gamma_matrix_approx); % can be used to
+%conveniently print out the matrix ..
