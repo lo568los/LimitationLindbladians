1699be3bf346eff224840eeb52f6380e0ec7b800
lo568los <soumyadeepsarma3@gmail.com>
2025-07-07 21:35:04 +0530

    correct

--- python/final_data_s.py
+++ python/final_data_s.py
@@ -166,11 +166,11 @@ def re_ness_g(beta_r,beta_l,s,g,ham_type,e):
             for k in range(number):
                     freq = eigenergies[k]-eigenergies[i]
                     if np.abs(freq) >= 1/10**10:
-                        constant12[i,k]=integral12[i,k]+integral11[i,k]+(0.5*(spectral_bath(eigenergies[k]-eigenergies[i],s,tb,gamma1)+func1(eigenergies[k]-eigenergies[i],s,tb,beta2,mu2,gamma1)))/np.abs(freq)   #full coefficient created this is nbar+1
-                        constant11[i,k]=integral11[i,k]+(0.5*func1(eigenergies[k]-eigenergies[i],s,tb,beta2,mu2,gamma1)) /np.abs(freq)                                  # the full coefficient is created
+                        constant12[i,k]=integral12[i,k]+integral11[i,k]+(0.5*(spectral_bath(eigenergies[k]-eigenergies[i],s,tb,gamma1)+func1(eigenergies[k]-eigenergies[i],s,tb,beta2,mu2,gamma1)))/(np.abs(freq)**(s-1))   #full coefficient created this is nbar+1
+                        constant11[i,k]=integral11[i,k]+(0.5*func1(eigenergies[k]-eigenergies[i],s,tb,beta2,mu2,gamma1)) /(np.abs(freq)**(s-1))                                   # the full coefficient is created
                         
-                        constant22[i,k]=integral22[i,k]+integral21[i,k]+(0.5*(spectral_bath(eigenergies[k]-eigenergies[i],s,tb,gamma2)+func1(eigenergies[k]-eigenergies[i],s,tb,beta1,mu1,gamma2)) )/np.abs(freq)   #full coefficient created this is nbar+1
-                        constant21[i,k]=integral21[i,k]+0.5*func1(eigenergies[k]-eigenergies[i],s,tb,beta1,mu1,gamma2)/np.abs(freq)   # the full coefficient is created
+                        constant22[i,k]=integral22[i,k]+integral21[i,k]+(0.5*(spectral_bath(eigenergies[k]-eigenergies[i],s,tb,gamma2)+func1(eigenergies[k]-eigenergies[i],s,tb,beta1,mu1,gamma2)) )/(np.abs(freq)**(s-1))    #full coefficient created this is nbar+1
+                        constant21[i,k]=integral21[i,k]+0.5*func1(eigenergies[k]-eigenergies[i],s,tb,beta1,mu1,gamma2)/(np.abs(freq)**(s-1))    # the full coefficient is created
                     #print(i,k,constant11[i,k],constant12[i,k],constant21[i,k],constant22[i,k])
 
 
